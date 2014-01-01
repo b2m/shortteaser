c0bf226d473c5a27f31a01e1e6dd018aa22dcb69
Benjamin Meier <gpl@code-meier.de>
2014-01-01 15:37:58 +0100

    Changed Copyright to 2013

--- ShortTeaser.php
+++ ShortTeaser.php
@@ -7,7 +7,7 @@
  * @category  Contao
  * @package   ShortTeaser
  * @author    Benjamin Meier <gpl@code-meier.de>
- * @copyright 2011 Benjamin Meier
+ * @copyright 2013 Benjamin Meier
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  * @link      http://www.code-meier.de
  */
@@ -20,7 +20,7 @@ if (!defined('TL_ROOT')) die('You can not access this file directly!');
  * @category  Contao
  * @package   ShortTeaser
  * @author    Benjamin Meier <gpl@code-meier.de>
- * @copyright 2011 Benjamin Meier
+ * @copyright 2013 Benjamin Meier
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  * @link      http://www.code-meier.de
  */
--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -7,7 +7,7 @@
  * @category  Contao
  * @package   ShortTeaser
  * @author    Benjamin Meier <gpl@code-meier.de>
- * @copyright 2011 Benjamin Meier
+ * @copyright 2013 Benjamin Meier
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  * @link      http://www.code-meier.de
  */
